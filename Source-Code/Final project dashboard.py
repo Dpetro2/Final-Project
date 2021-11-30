a37f8255bf3aa275d6b355bdddbd3d7ea14f07c6
Dpetro2 <89052272+Dpetro2@users.noreply.github.com>
2021-11-29 19:37:41 -0500

    updated source code file
    this file includes the bar graph with slider page

--- Source-Code/Final project dashboard.py
+++ Source-Code/Final project dashboard.py
@@ -58,68 +58,68 @@ stateList = [{'label': 'Alabama', 'value': 'AL'},
              {'label': 'Wisconsin', 'value': 'WI'},
              {'label': 'Wyoming', 'value': 'WY'}]
 yearList = [{'label': '1960', 'value': '1960'},
-{'label': '1961', 'value': '1961'},
-{'label': '1962', 'value': '1962'},
-{'label': '1963', 'value': '1963'},
-{'label': '1964', 'value': '1964'},
-{'label': '1965', 'value': '1965'},
-{'label': '1966', 'value': '1966'},
-{'label': '1967', 'value': '1967'},
-{'label': '1968', 'value': '1968'},
-{'label': '1969', 'value': '1969'},
-{'label': '1970', 'value': '1970'},
-{'label': '1971', 'value': '1971'},
-{'label': '1972', 'value': '1972'},
-{'label': '1973', 'value': '1973'},
-{'label': '1974', 'value': '1974'},
-{'label': '1975', 'value': '1975'},
-{'label': '1976', 'value': '1976'},
-{'label': '1977', 'value': '1977'},
-{'label': '1978', 'value': '1978'},
-{'label': '1979', 'value': '1979'},
-{'label': '1980', 'value': '1980'},
-{'label': '1981', 'value': '1981'},
-{'label': '1982', 'value': '1982'},
-{'label': '1983', 'value': '1983'},
-{'label': '1984', 'value': '1984'},
-{'label': '1985', 'value': '1985'},
-{'label': '1986', 'value': '1986'},
-{'label': '1987', 'value': '1987'},
-{'label': '1988', 'value': '1988'},
-{'label': '1989', 'value': '1989'},
-{'label': '1990', 'value': '1990'},
-{'label': '1991', 'value': '1991'},
-{'label': '1992', 'value': '1992'},
-{'label': '1993', 'value': '1993'},
-{'label': '1994', 'value': '1994'},
-{'label': '1995', 'value': '1995'},
-{'label': '1996', 'value': '1996'},
-{'label': '1997', 'value': '1997'},
-{'label': '1998', 'value': '1998'},
-{'label': '1999', 'value': '1999'},
-{'label': '2000', 'value': '2000'},
-{'label': '2001', 'value': '2001'},
-{'label': '2002', 'value': '2002'},
-{'label': '2003', 'value': '2003'},
-{'label': '2004', 'value': '2004'},
-{'label': '2005', 'value': '2005'},
-{'label': '2006', 'value': '2006'},
-{'label': '2007', 'value': '2007'},
-{'label': '2008', 'value': '2008'},
-{'label': '2009', 'value': '2009'},
-{'label': '2010', 'value': '2010'},
-{'label': '2011', 'value': '2011'},
-{'label': '2012', 'value': '2012'},
-{'label': '2013', 'value': '2013'},
-{'label': '2014', 'value': '2014'},
-{'label': '2015', 'value': '2015'},
-{'label': '2016', 'value': '2016'},
-{'label': '2017', 'value': '2017'},
-{'label': '2018', 'value': '2018'},
-{'label': '2019', 'value': '2019'}]
+            {'label': '1961', 'value': '1961'},
+            {'label': '1962', 'value': '1962'},
+            {'label': '1963', 'value': '1963'},
+            {'label': '1964', 'value': '1964'},
+            {'label': '1965', 'value': '1965'},
+            {'label': '1966', 'value': '1966'},
+            {'label': '1967', 'value': '1967'},
+            {'label': '1968', 'value': '1968'},
+            {'label': '1969', 'value': '1969'},
+            {'label': '1970', 'value': '1970'},
+            {'label': '1971', 'value': '1971'},
+            {'label': '1972', 'value': '1972'},
+            {'label': '1973', 'value': '1973'},
+            {'label': '1974', 'value': '1974'},
+            {'label': '1975', 'value': '1975'},
+            {'label': '1976', 'value': '1976'},
+            {'label': '1977', 'value': '1977'},
+            {'label': '1978', 'value': '1978'},
+            {'label': '1979', 'value': '1979'},
+            {'label': '1980', 'value': '1980'},
+            {'label': '1981', 'value': '1981'},
+            {'label': '1982', 'value': '1982'},
+            {'label': '1983', 'value': '1983'},
+            {'label': '1984', 'value': '1984'},
+            {'label': '1985', 'value': '1985'},
+            {'label': '1986', 'value': '1986'},
+            {'label': '1987', 'value': '1987'},
+            {'label': '1988', 'value': '1988'},
+            {'label': '1989', 'value': '1989'},
+            {'label': '1990', 'value': '1990'},
+            {'label': '1991', 'value': '1991'},
+            {'label': '1992', 'value': '1992'},
+            {'label': '1993', 'value': '1993'},
+            {'label': '1994', 'value': '1994'},
+            {'label': '1995', 'value': '1995'},
+            {'label': '1996', 'value': '1996'},
+            {'label': '1997', 'value': '1997'},
+            {'label': '1998', 'value': '1998'},
+            {'label': '1999', 'value': '1999'},
+            {'label': '2000', 'value': '2000'},
+            {'label': '2001', 'value': '2001'},
+            {'label': '2002', 'value': '2002'},
+            {'label': '2003', 'value': '2003'},
+            {'label': '2004', 'value': '2004'},
+            {'label': '2005', 'value': '2005'},
+            {'label': '2006', 'value': '2006'},
+            {'label': '2007', 'value': '2007'},
+            {'label': '2008', 'value': '2008'},
+            {'label': '2009', 'value': '2009'},
+            {'label': '2010', 'value': '2010'},
+            {'label': '2011', 'value': '2011'},
+            {'label': '2012', 'value': '2012'},
+            {'label': '2013', 'value': '2013'},
+            {'label': '2014', 'value': '2014'},
+            {'label': '2015', 'value': '2015'},
+            {'label': '2016', 'value': '2016'},
+            {'label': '2017', 'value': '2017'},
+            {'label': '2018', 'value': '2018'},
+            {'label': '2019', 'value': '2019'}]
+
 # Load csv into dataframe
 df = pd.read_csv('C:/Users/jauga/PycharmProjects/Final_Prject/Database/Complete_SEDS_only_RC.csv')
-
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
@@ -184,20 +184,21 @@ app.layout = dbc.Container([
     [Input('url', 'pathname')]
 )
 def render_page_content(pathname):
-#About page
+    # About page
     if pathname == '/':
         return [
             dbc.Container([
                 dbc.Row([
                     dbc.Col([
 
-                        html.B('this is gonna be a hella long about page that introduces the idea of our project, even if our idea is bad')
+                        html.B(
+                            'this is gonna be a hella long about page that introduces the idea of our project, even if our idea is bad')
                     ])
                 ])
             ])
 
         ]
-#Line graph page
+    # Line graph page
     elif pathname == '/page-1':
         return [
             dbc.Container([
@@ -222,23 +223,39 @@ def render_page_content(pathname):
                 ])
             ])
         ]
-# Bar graph page
+    # Bar graph page
     elif pathname == '/page-2':
-        return[
+        return [
             dbc.Container([
                 dbc.Row([
                     dbc.Col([
-                        dcc.Dropdown(id='year-selector-dropdown',
-                                     placeholder='Year',
-                                     options=yearList,
-                                     className='text-dark',
-                                     clearable=False,
-                                     value='2019',
-                                     style={'background-color': '#FF6544'})
-                    ]),
-                    dbc.Col([
                         html.H2('Bar graph showing very cool data!', style={'textAlign': 'center'}),
                         dcc.Graph(id='bar-graph')
+                    ])
+                ]),
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Slider(id='year-selector-slider',
+                                   min=1960,
+                                   max=2019,
+                                   step=1,
+                                   value=2019,
+                                   className='bg-primary',
+                                   marks={
+                                       1960: {'label': "1960", 'style':{'color': '#FF6544'}},
+                                       1965: {'label': "1965", 'style':{'color': '#FF6544'}},
+                                       1970: {'label': "1970", 'style':{'color': '#FF6544'}},
+                                       1975: {'label': "1975", 'style':{'color': '#FF6544'}},
+                                       1980: {'label': "1980", 'style':{'color': '#FF6544'}},
+                                       1985: {'label': "1985", 'style':{'color': '#FF6544'}},
+                                       1990: {'label': "1990", 'style':{'color': '#FF6544'}},
+                                       1995: {'label': "1995", 'style':{'color': '#FF6544'}},
+                                       2000: {'label': "2000", 'style':{'color': '#FF6544'}},
+                                       2005: {'label': "2005", 'style':{'color': '#FF6544'}},
+                                       2010: {'label': "2010", 'style':{'color': '#FF6544'}},
+                                       2015: {'label': "2015", 'style':{'color': '#FF6544'}},
+                                       2019: {'label': "2019", 'style':{'color': '#FF6544'}}
+                                   })
                     ])
                 ])
             ])
@@ -269,17 +286,18 @@ def update_figure(selectedState):
                                  yaxis={'title': 'Dollars per Million BTU'})
              }]
 
+
 @app.callback(
     Output('bar-graph', 'figure'),
-    Input('year-selector-dropdown', 'value')
+    Input('year-selector-slider', 'value')
 )
 def update_figure(selectedYear):
-    filtered_df1 = df[df['year'] == selectedYear]
+    filtered_df1 = df[df['Year'] == selectedYear]
     filtered_df1 = filtered_df1[filtered_df1['MSN'] == 'SORCB']
 
     bar_graph_data = [go.Bar(x=filtered_df1['state'], y=filtered_df1['data'])]
     return {'data': bar_graph_data,
-            'layout': go.layout(title='Residential Solar Energy Consumed by State in ' + selectedYear,
+            'layout': go.Layout(title='Residential Solar Energy Consumed by State in ' ,
                                 xaxis={'title': 'State'},
                                 yaxis={'title': 'Billion Btu'})}
 
